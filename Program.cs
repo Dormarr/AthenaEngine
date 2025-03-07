d9a53e2de4e57e4eb700bbe22b7b2f3668227c38
dormarr <theyranappleyard@gmail.com>
2025-03-07 20:47:29 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Graphics.OpenGL4;
+using System;
+
+class Game : GameWindow
+{
+    public Game(GameWindowSettings gws, NativeWindowSettings nws) : base(gws, nws) { }
+
+    protected override void OnLoad()
+    {
+        base.OnLoad();
+        GL.ClearColor(0.5f, 0.5f, 0.5f, 1.0f); // Set background color
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs args)
+    {
+        base.OnRenderFrame(args);
+        GL.Clear(ClearBufferMask.ColorBufferBit);
+        SwapBuffers(); // Render the frame
+    }
+
+    public static void Main()
+    {
+        var game = new Game(GameWindowSettings.Default, new NativeWindowSettings { Size = new OpenTK.Mathematics.Vector2i(800, 600), Title = "My Game Engine" });
+        game.Run();
+    }
+}
